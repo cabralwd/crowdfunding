7478ca7a0bbef64436dcaf336771240d3f8133b8
cbral <cbraljunior@icloud.com>
2021-01-02 22:40:28 -0300

    function tab navegation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,3 +42,32 @@ $(document).ready(function () {
     }
   });
 });
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+  tabs[0].classList.add('activeTab');
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(tabContent => {
+      tabContent.style.display = "none";
+      tabContent.style.transform = "translateY(30px)";
+      tabContent.style.opacity = "0";
+
+      tabs.forEach(thow => {
+        thow.classList.remove('activeTab');
+
+        if (!tab.classList.contains('activeTab')) {
+          tab.classList.add('activeTab');
+        }
+      })
+    });
+
+    target.style.display = "block";
+    setTimeout(() => {
+      target.style.opacity = "1";
+      target.style.transform = "translateY(0px)";
+    }, 100)
+  })
+})
